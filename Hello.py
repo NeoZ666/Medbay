ec218de5a00b2e23ca713420afa316713002aacd
Harsh <115883668+NeoZ666@users.noreply.github.com>
2024-04-04 18:34:34 +0000

    draft 4

--- Hello.py
+++ Hello.py
@@ -1,26 +1,24 @@
 import streamlit as st
 
+def predict_drug(age, sex, bp, cholesterol, na_k):
+    input_features = [[age, sex, bp, cholesterol, na_k]]
+    prediction = model.predict(input_features)
+    return prediction
+
 st.title('Drug prediction')
+
 with st.form("my_form"):    
-    age = st.slider("Select Age",min_value=0, max_value=120, value=20)
+    age = st.slider("Select Age", min_value=0, max_value=120, value=20)
+    na_k = st.slider("Enter Sodium to potassium ratio in blood (Na to k)", min_value=6.000, max_value=39.000, value=6.000)
+    sex = st.radio("Sex", options=["Male", "Female"])
+    bp = st.radio("Blood Pressure", options=["Low", "Medium", "High"])
+    cholesterol = st.radio("Cholesterol", options=["Medium", "High"])
 
-    Na_k = st.slider("Enter Sodium to potassium ratio in blood (Na to k)",min_value=6.000, max_value=39.000, value=6.000)
+    submitted = st.form_submit_button("Submit")
 
-    sex = st.radio(
-        "Sex",
-        key="visibility",
-        options=["Male", "Female"],
-    )
-
-    bp = st.radio(
-        "Blood Pressure",
-        options=["Low", "Medium", "High"],
-    )
-
-    Cholesterol = st.radio(
-        "Cholesterol",
-        options=["Low", "Medium", "High"],
-    )
-
-
-    st.form_submit_button(label="Submit")
+if submitted:
+    sex = 1 if sex == "Male" else 0
+    bp = {"Low": 0, "Medium": 1, "High": 2}[bp]
+    cholesterol = {"Low": 0, "Medium": 1, "High": 2}[cholesterol]
+    drug = predict_drug(age, sex, bp, cholesterol, na_k)
+    st.write("Predicted drug:", drug)
